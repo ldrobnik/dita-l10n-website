1f6dc0b98f2286394bf5cf68b893341725174749
Łukasz Drobnik <drobniklukasz@gmail.com>
2021-04-22 16:30:57 +0200

    Update website & PDF

--- oxygen-webhelp/app/config.js
+++ oxygen-webhelp/app/config.js
@@ -105,7 +105,7 @@ define(function() {
         	
             if(id === 'index-1' || id === 'index-2' || id === 'index-3' || id === 'stopwords' 
             ||id === 'htmlFileInfoList' || id === 'keywords') {
-                args = '20210422155421';
+                args = '20210422163025';
         	} 
 	
 	        return (url.indexOf('?') === -1 ? '?' : '&') + args;
--- oxygen-webhelp/app/search/index/keywords.js
+++ oxygen-webhelp/app/search/index/keywords.js
@@ -1,19 +1,21 @@
-define(function() {var keywords=[{w:"Conditional",p:["p0"]},{w:"text",p:["p0","p4","p5","p8"]},{w:"Content",p:["p1"]},{w:"reuse",p:["p1"]},{w:"Glossaries",p:["p2"]},{w:"Localization-friendly",p:["p3"]},{w:"images",p:["p3"]},{w:"Non-translatable",p:["p4"]},{w:"Translation-friendly",p:["p5"]},{w:"UI",p:["p6"]},{w:"strings",p:["p6"]},{w:"Localization-related",p:["p7"]},{w:"attributes",p:["p7"]},{w:"sorting",p:["p8"]},{w:"Organizing",p:["p9"]},{w:"your",p:["p9"]},{w:"project",p:["p9","p11"]},{w:"structure",p:["p9"]},{w:"Resolving",p:["p10"]},{w:"conrefs,",p:["p10"]},{w:"keyrefs,",p:["p10"]},{w:"and",p:["p10"]},{w:"conkeyrefs",p:["p10"]},{w:"Importing",p:["p11","p12"]},{w:"a",p:["p11","p12"]},{w:"DITA",p:["p11"]},{w:"into",p:["p11","p12"]},{w:"memoQ",p:["p11"]},{w:"terms",p:["p12"]},{w:"term",p:["p12"]},{w:"base",p:["p12"]},{w:"Translating",p:["p13"]},{w:"repetitions",p:["p13"]}];
+define(function() {var keywords=[{w:"How",p:["p0"]},{w:"to",p:["p0"]},{w:"prepare",p:["p0"]},{w:"DITA",p:["p0","p13"]},{w:"content",p:["p0","p2","p5"]},{w:"for",p:["p0"]},{w:"localization",p:["p0"]},{w:"Conditional",p:["p1"]},{w:"text",p:["p1","p6","p7","p10"]},{w:"reuse",p:["p2"]},{w:"Glossaries",p:["p3"]},{w:"Localization-friendly",p:["p4"]},{w:"images",p:["p4"]},{w:"Non-translatable",p:["p5","p6"]},{w:"Translation-friendly",p:["p7"]},{w:"UI",p:["p8"]},{w:"strings",p:["p8"]},{w:"Localization-related",p:["p9"]},{w:"attributes",p:["p9"]},{w:"sorting",p:["p10"]},{w:"Organizing",p:["p11"]},{w:"your",p:["p11"]},{w:"project",p:["p11","p13"]},{w:"structure",p:["p11"]},{w:"Resolving",p:["p12"]},{w:"conrefs,",p:["p12"]},{w:"keyrefs,",p:["p12"]},{w:"and",p:["p12"]},{w:"conkeyrefs",p:["p12"]},{w:"Importing",p:["p13","p14"]},{w:"a",p:["p13","p14"]},{w:"into",p:["p13","p14"]},{w:"memoQ",p:["p13"]},{w:"terms",p:["p14"]},{w:"term",p:["p14"]},{w:"base",p:["p14"]},{w:"Translating",p:["p15"]},{w:"repetitions",p:["p15"]}];
 var ph={};
-ph["p0"]=[0, 1];
-ph["p1"]=[2, 3];
-ph["p2"]=[4];
-ph["p3"]=[5, 6];
-ph["p4"]=[7, 1];
-ph["p5"]=[8, 1];
-ph["p6"]=[9, 10];
-ph["p7"]=[11, 12];
-ph["p8"]=[1, 13];
-ph["p9"]=[14, 15, 16, 17];
-ph["p10"]=[18, 19, 20, 21, 22];
-ph["p12"]=[23, 28, 26, 24, 29, 30];
-ph["p11"]=[23, 24, 25, 16, 26, 27];
-ph["p13"]=[31, 32];
+ph["p0"]=[0, 1, 2, 3, 4, 5, 6];
+ph["p1"]=[7, 8];
+ph["p2"]=[4, 9];
+ph["p3"]=[10];
+ph["p4"]=[11, 12];
+ph["p5"]=[13, 4];
+ph["p6"]=[13, 8];
+ph["p7"]=[14, 8];
+ph["p8"]=[15, 16];
+ph["p9"]=[17, 18];
+ph["p10"]=[8, 19];
+ph["p12"]=[24, 25, 26, 27, 28];
+ph["p11"]=[20, 21, 22, 23];
+ph["p14"]=[29, 33, 31, 30, 34, 35];
+ph["p13"]=[29, 30, 3, 22, 31, 32];
+ph["p15"]=[36, 37];
      return {
          keywords: keywords,
          ph: ph
