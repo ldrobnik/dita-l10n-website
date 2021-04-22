1f01b1b15d59d41970a5bfdcc2c14ae163c50a51
Łukasz Drobnik <drobniklukasz@gmail.com>
2021-04-22 15:55:37 +0200

    Create website

---
+++ oxygen-webhelp/app/search/index/keywords.js
@@ -0,0 +1,21 @@
+define(function() {var keywords=[{w:"Conditional",p:["p0"]},{w:"text",p:["p0","p4","p5","p8"]},{w:"Content",p:["p1"]},{w:"reuse",p:["p1"]},{w:"Glossaries",p:["p2"]},{w:"Localization-friendly",p:["p3"]},{w:"images",p:["p3"]},{w:"Non-translatable",p:["p4"]},{w:"Translation-friendly",p:["p5"]},{w:"UI",p:["p6"]},{w:"strings",p:["p6"]},{w:"Localization-related",p:["p7"]},{w:"attributes",p:["p7"]},{w:"sorting",p:["p8"]},{w:"Organizing",p:["p9"]},{w:"your",p:["p9"]},{w:"project",p:["p9","p11"]},{w:"structure",p:["p9"]},{w:"Resolving",p:["p10"]},{w:"conrefs,",p:["p10"]},{w:"keyrefs,",p:["p10"]},{w:"and",p:["p10"]},{w:"conkeyrefs",p:["p10"]},{w:"Importing",p:["p11","p12"]},{w:"a",p:["p11","p12"]},{w:"DITA",p:["p11"]},{w:"into",p:["p11","p12"]},{w:"memoQ",p:["p11"]},{w:"terms",p:["p12"]},{w:"term",p:["p12"]},{w:"base",p:["p12"]},{w:"Translating",p:["p13"]},{w:"repetitions",p:["p13"]}];
+var ph={};
+ph["p0"]=[0, 1];
+ph["p1"]=[2, 3];
+ph["p2"]=[4];
+ph["p3"]=[5, 6];
+ph["p4"]=[7, 1];
+ph["p5"]=[8, 1];
+ph["p6"]=[9, 10];
+ph["p7"]=[11, 12];
+ph["p8"]=[1, 13];
+ph["p9"]=[14, 15, 16, 17];
+ph["p10"]=[18, 19, 20, 21, 22];
+ph["p12"]=[23, 28, 26, 24, 29, 30];
+ph["p11"]=[23, 24, 25, 16, 26, 27];
+ph["p13"]=[31, 32];
+     return {
+         keywords: keywords,
+         ph: ph
+     }
+});
