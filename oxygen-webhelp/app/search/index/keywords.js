77eedd7b291fd47a100502f2cc687aeeaee48a05
Łukasz Drobnik <drobniklukasz@gmail.com>
2021-04-23 11:10:24 +0200

    Update website & PDF

--- oxygen-webhelp/app/search/index/keywords.js
+++ oxygen-webhelp/app/search/index/keywords.js
@@ -1,4 +1,4 @@
-define(function() {var keywords=[{w:"How",p:["p0"]},{w:"to",p:["p0"]},{w:"prepare",p:["p0"]},{w:"DITA",p:["p0","p13"]},{w:"content",p:["p0","p2","p5"]},{w:"for",p:["p0"]},{w:"localization",p:["p0"]},{w:"Conditional",p:["p1"]},{w:"text",p:["p1","p6","p7","p10"]},{w:"reuse",p:["p2"]},{w:"Glossaries",p:["p3"]},{w:"Localization-friendly",p:["p4"]},{w:"images",p:["p4"]},{w:"Non-translatable",p:["p5","p6"]},{w:"Translation-friendly",p:["p7"]},{w:"UI",p:["p8"]},{w:"strings",p:["p8"]},{w:"Localization-related",p:["p9"]},{w:"attributes",p:["p9"]},{w:"sorting",p:["p10"]},{w:"Organizing",p:["p11"]},{w:"your",p:["p11"]},{w:"project",p:["p11","p13"]},{w:"structure",p:["p11"]},{w:"Resolving",p:["p12"]},{w:"conrefs,",p:["p12"]},{w:"keyrefs,",p:["p12"]},{w:"and",p:["p12"]},{w:"conkeyrefs",p:["p12"]},{w:"Importing",p:["p13","p14"]},{w:"a",p:["p13","p14"]},{w:"into",p:["p13","p14"]},{w:"memoQ",p:["p13"]},{w:"terms",p:["p14"]},{w:"term",p:["p14"]},{w:"base",p:["p14"]},{w:"Translating",p:["p15"]},{w:"repetitions",p:["p15"]}];
+define(function() {var keywords=[{w:"How",p:["p0"]},{w:"to",p:["p0"]},{w:"prepare",p:["p0"]},{w:"DITA",p:["p0","p14"]},{w:"content",p:["p0","p2","p5"]},{w:"for",p:["p0"]},{w:"localization",p:["p0"]},{w:"Conditional",p:["p1"]},{w:"text",p:["p1","p6","p7","p9","p11"]},{w:"reuse",p:["p2"]},{w:"Glossaries",p:["p3"]},{w:"Localization-friendly",p:["p4"]},{w:"images",p:["p4"]},{w:"Non-translatable",p:["p5","p6"]},{w:"Translation-friendly",p:["p7","p9"]},{w:"UI",p:["p8"]},{w:"strings",p:["p8"]},{w:"Writing",p:["p9"]},{w:"Localization-related",p:["p10"]},{w:"attributes",p:["p10"]},{w:"sorting",p:["p11"]},{w:"Organizing",p:["p12"]},{w:"your",p:["p12"]},{w:"project",p:["p12","p14"]},{w:"structure",p:["p12"]},{w:"Resolving",p:["p13"]},{w:"conrefs,",p:["p13"]},{w:"keyrefs,",p:["p13"]},{w:"and",p:["p13"]},{w:"conkeyrefs",p:["p13"]},{w:"Importing",p:["p14","p15"]},{w:"a",p:["p14","p15"]},{w:"into",p:["p14","p15"]},{w:"memoQ",p:["p14"]},{w:"terms",p:["p15"]},{w:"term",p:["p15"]},{w:"base",p:["p15"]},{w:"Translating",p:["p16"]},{w:"repetitions",p:["p16"]}];
 var ph={};
 ph["p0"]=[0, 1, 2, 3, 4, 5, 6];
 ph["p1"]=[7, 8];
@@ -9,13 +9,14 @@ ph["p5"]=[13, 4];
 ph["p6"]=[13, 8];
 ph["p7"]=[14, 8];
 ph["p8"]=[15, 16];
-ph["p9"]=[17, 18];
-ph["p10"]=[8, 19];
-ph["p12"]=[24, 25, 26, 27, 28];
-ph["p11"]=[20, 21, 22, 23];
-ph["p14"]=[29, 33, 31, 30, 34, 35];
-ph["p13"]=[29, 30, 3, 22, 31, 32];
-ph["p15"]=[36, 37];
+ph["p9"]=[17, 14, 8];
+ph["p10"]=[18, 19];
+ph["p12"]=[21, 22, 23, 24];
+ph["p11"]=[8, 20];
+ph["p14"]=[30, 31, 3, 23, 32, 33];
+ph["p13"]=[25, 26, 27, 28, 29];
+ph["p16"]=[37, 38];
+ph["p15"]=[30, 34, 32, 31, 35, 36];
      return {
          keywords: keywords,
          ph: ph
