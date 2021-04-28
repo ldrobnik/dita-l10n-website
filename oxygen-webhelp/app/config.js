fa57683dba364d9ca7118e2acafc9d57d4ba00e1
Łukasz Drobnik <drobniklukasz@gmail.com>
2021-04-29 01:27:24 +0200

    Update website and PDF

--- oxygen-webhelp/app/config.js
+++ oxygen-webhelp/app/config.js
@@ -105,7 +105,7 @@ define(function() {
         	
             if(id === 'index-1' || id === 'index-2' || id === 'index-3' || id === 'stopwords' 
             ||id === 'htmlFileInfoList' || id === 'keywords') {
-                args = '20210429011611';
+                args = '20210429011917';
         	} 
 	
 	        return (url.indexOf('?') === -1 ? '?' : '&') + args;
